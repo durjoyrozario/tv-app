95d3115de1d7baa49249ce00d1b2e08c4faecc4c
durjoyrozario <88568472+durjoyrozario@users.noreply.github.com>
2026-06-16 14:54:43 +0600

    Create BootReceiver.java

---
+++ app/src/main/java/com/durjoy/tv/BootReceiver.java
@@ -0,0 +1,17 @@
+package com.durjoy.tv;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        if (Intent.ACTION_BOOT_COMPLETED.equals(action) ||
+            "android.intent.action.QUICKBOOT_POWERON".equals(action)) {
+            Intent launchIntent = new Intent(context, MainActivity.class);
+            launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            launchIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            context.startActivity(launchIntent);
+        }
+    }
+}
